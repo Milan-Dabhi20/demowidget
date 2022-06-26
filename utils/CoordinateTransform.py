c542f2d660921b6582b412a4a2a5b3fd4c9455b1
Milan Dabhi <developer@acodesoft.com>
2022-06-26 23:05:17 +0200

    created new json file for testing

---
+++ utils/CoordinateTransform.py
@@ -0,0 +1,35 @@
+import json
+import os
+from numpy import add
+import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+f = open(os.path.join(BASE_DIR ,'rennes/Addresses.geojson'), "r")
+f2 = open(os.path.join(BASE_DIR ,'assets/data/simulation_playback_traffic.json'), "r")
+data = json.load(f)
+addressCoordinates = []
+
+for feature in data["features"]:
+    addressCoordinates.append(feature["geometry"]["coordinates"])
+    if len(addressCoordinates) == 500:
+        break
+ #   print(feature["geometry"]["coordinates"])
+
+
+trafficData = json.load(f2)
+print(len(trafficData))
+newTrafficJson = []
+for eachData, eachCoordinate in zip(trafficData, addressCoordinates):
+    newDict = {"timestamp":eachData["timestamp"],
+                "geoJson":{"type":"FeatureCollection",
+                        "features":[{"feature_type":"SUV", 
+                                     "coordinates":eachCoordinate,
+                                     "feature_id":1,
+                                     "orientation":eachData["geoJson"]["features"][0]["orientations"][2],
+                                     "type":"Point"}]}}
+    newTrafficJson.append(newDict)
+    
+jsonString = json.dumps(newTrafficJson)
+jsonFile = open(os.path.join(BASE_DIR ,'assets/data/sample_traffic.json'), "w")
+jsonFile.write(jsonString)
+jsonFile.close()
